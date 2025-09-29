3c455cc2373919872ad4237a7f62e812034327eb
Gabriel Walter Fuchsberger <gabriel-walter@hotmail.com>
2025-09-29 16:59:21 -0300

    exercício para comer doce

---
+++ cansadin.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int hora, minuto;
+    printf("Que horas são? (formato HH MM): ");
+    scanf("%d %d", &hora, &minuto);
+
+    printf("É hora de comer um doce!\n");
+    return 0;
+}
