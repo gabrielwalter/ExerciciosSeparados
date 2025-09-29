aa5f7a58feace6c724c495c05ea581948462f0a8
Gabriel Walter <gabriel-walter@hotmail.com>
2025-09-29 13:36:16 -0300

    Exercícios para a P1 de LPG

---
+++ Ex01_Soma/Ex01_Soma.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+
+    setlocale(LC_ALL, "");
+
+    printf("Olá, mundo! hihi\n"); // remover
+
+
+
+
+
+    return 0;
+}
---
+++ GabiELucca/GabiELucca.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+    //Para problemas de acento
+    setlocale(LC_ALL, "");
+    while (1 == 1) {
+
+        printf("Gabriel Walter e Lucca amizade eterna\n");
+    }
+
+}
