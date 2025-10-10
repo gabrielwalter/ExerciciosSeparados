24a8d0315737af96df94e8f921ae5f5dc540d87a
Gabriel Walter Fuchsberger <gabriel-walter@hotmail.com>
2025-10-10 14:48:02 -0300

    Adiciona configuração UTF-8 multiplataforma em todos os exercícios

--- Ex02_Media/main.c
+++ Ex02_Media/main.c
@@ -1,6 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <locale.h>
+#ifdef _WIN32
+    #include <windows.h>
+#endif
 #include "ex02.c"
 
 int main() {
+    // Configura o console para UTF-8
+    #ifdef _WIN32
+        SetConsoleOutputCP(65001);
+        SetConsoleCP(65001);
+    #endif
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
     Ex02_Media();
     return 0;
 }
